ab9749dd030bfdf7839026aea347f3d7b2750bdf
ianhhhhhhhhe <517382432@qq.com>
2018-09-19 20:04:46 +0800

    Update: remove min_interval

--- start.js
+++ start.js
@@ -33,7 +33,6 @@ var appDataDir = desktopApp.getAppDataDir();
 var KEYS_FILENAME = appDataDir + '/' + (conf.KEYS_FILENAME || 'keys.json');
 var wallet_id;
 var xPrivKey;
-var MIN_INTERVAL = conf.MIN_INTERVAL || 60 * 1000;
 
 
 function replaceConsoleLog(){
