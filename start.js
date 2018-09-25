fd8ae1910fb2c78042f66a04b18cf66ad5713dda
ianhhhhhhhhe <517382432@qq.com>
2018-09-25 10:14:16 +0800

    Update: add compare version which is removed accidentally

--- start.js
+++ start.js
@@ -707,6 +707,14 @@ eventBus.on('headless_wallet_ready', function(){
 		console.log('=== Round Switch === : '+round_index);
 	})
 	
+	eventBus.on('peer_version', function (ws, body) {
+		if (body.program == conf.clientName) {
+			if (conf.minClientVersion && compareVersions(body.program_version, conf.minClientVersion) == '<')
+				network.sendJustsaying(ws, 'new_version', {version: conf.minClientVersion});
+			if (compareVersions(body.program_version, '1.5.1') == '<')
+				ws.close(1000, "mandatory upgrade");
+		}
+	});
 
 	setInterval(function(){
 		console.log(`Mining Status: ${bMining}, POW Status: ${bPowSent}  ready to checkTrustMEAndStartMinig`)
