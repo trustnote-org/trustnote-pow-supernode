7f7393701ddfe4c36da7a7041ad9b64910c305d0
ianhhhhhhhhe <517382432@qq.com>
2018-10-10 19:31:07 +0800

    Update: Add a conf file, for dev only

---
+++ conf-dev.js
@@ -0,0 +1,45 @@
+/*jslint node: true */
+"use strict";
+
+exports.clientName = 'TTT';
+exports.minClientVersion = '1.1.0';
+exports.WS_PROTOCOL = 'ws://';
+// https://console.developers.google.com
+exports.pushApiProjectNumber = 0;
+exports.pushApiKey = '';
+
+exports.port = 9191;
+//exports.myUrl = 'ws://10.10.11.68:9191';
+exports.bServeAsHub = true;
+exports.bSaveJointJson = true;
+exports.bLight = false;
+exports.bServeAsRpc = false;
+
+// this is used by wallet vendor only, to redirect bug reports to developers' email
+exports.bug_sink_email = 'admin@example.org';
+exports.bugs_from_email = 'bugs@example.org';
+
+exports.HEARTBEAT_TIMEOUT = 300*1000;
+
+exports.initial_peers = [
+    "ws://dev.mainchain.pow.trustnote.org:9191",
+];
+
+exports.storage = 'sqlite';
+
+exports.deviceName = 'Supernode';
+exports.permanent_pairing_secret = 'randomstring';
+exports.control_addresses = ['DEVICE ALLOWED TO CHAT'];
+exports.payout_address = 'WHERE THE MONEY CAN BE SENT TO';
+
+exports.bSingleAddress = true;
+exports.THRESHOLD_DISTANCE = 7;
+exports.MIN_AVAILABLE_WITNESSINGS = 100;
+exports.bPostTimestamp = false;
+
+exports.start_mining_round = 1000;
+exports.maxWorkderCount = 0;
+
+exports.KEYS_FILENAME = 'keys.json';
+
+console.log('finished witness conf');
