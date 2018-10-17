a0d4c9c8f6462c7836ca2f705f9dfd68eacdfc38
ianhhhhhhhhe <517382432@qq.com>
2018-10-17 18:00:50 +0800

    Update: exports.rpcInterface = '127.0.0.1';

--- conf.js
+++ conf.js
@@ -19,6 +19,7 @@ exports.bServeAsHub = true;
 exports.bSaveJointJson = true;
 exports.bLight = false;
 exports.bServeAsRpc = true;
+exports.rpcInterface = '127.0.0.1';
 exports.rpcPort = 6553;
 
 // this is used by wallet vendor only, to redirect bug reports to developers' email
