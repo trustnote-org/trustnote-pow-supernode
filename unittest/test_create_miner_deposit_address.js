44e90c48f129adabafe30ca53b4e9ddc535bcc72
ianhhhhhhhhe <517382432@qq.com>
2018-10-25 16:28:25 +0800

    Update: lib unitttest
    Rename create_miner_shared_address => create_deposit_address, and rename function name. Update unittest

---
+++ unittest/test_create_miner_deposit_address.js
@@ -0,0 +1,10 @@
+require('../start.js');
+var eventBus = require('trustnote-pow-common/base/event_bus');
+
+eventBus.on('headless_wallet_ready', function(){
+    var createMinerDepositAddress = require('../lib/create_deposit_address.js');
+
+    createMinerDepositAddress.createMinerDepositAddress('RV5KBMMUBQKG6EKRSKLFOSJOMVBUFOHU', 1000, function(shared_address){
+        console.log(JSON.stringify(shared_address));
+    })
+})
