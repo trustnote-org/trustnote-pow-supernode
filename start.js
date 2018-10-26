aa2f5006ef8621dd97a0f4e7141a9f5142491a22
ianhhhhhhhhe <517382432@qq.com>
2018-10-26 10:36:02 +0800

    Fix: update signer and start to fix xPrivKey is not defined

--- start.js
+++ start.js
@@ -1,10 +1,6 @@
 /*jslint node: true */
 "use strict";
-var fs = require('fs');
-var crypto = require('crypto');
 var Mnemonic = require('bitcore-mnemonic');
-var Bitcore = require('bitcore-lib');
-var readline = require('readline');
 
 var constants = require('trustnote-pow-common/config/constants.js');
 var conf = require('trustnote-pow-common/config/conf.js');
@@ -29,8 +25,6 @@ var bWitnessingUnderWay = false;
 var forcedWitnessingTimer;
 var count_witnessings_available = 0;
 var last_round_index = 0;
-var appDataDir = desktopApp.getAppDataDir();
-var KEYS_FILENAME = appDataDir + '/' + (conf.KEYS_FILENAME || 'keys.json');
 var wallet_id;
 var xPrivKey;
 var interval;
@@ -46,107 +40,6 @@ function onMiningError(err){
 	// throw Error(err);
 	bMining = false;
 	console.log("Mining Error: " + err);
-}
-
-function readKeys(onDone){
-	console.log('-----------------------');
-	if (conf.control_addresses)
-		console.log("remote access allowed from devices: "+conf.control_addresses.join(', '));
-	if (conf.payout_address)
-		console.log("payouts allowed to address: "+conf.payout_address);
-	console.log('-----------------------');
-	fs.readFile(KEYS_FILENAME, 'utf8', function(err, data){
-		var rl = readline.createInterface({
-			input: process.stdin,
-			output: process.stdout,
-			//terminal: true
-		});
-		if (err){ // first start
-			console.log('failed to read keys, will gen');
-			var suggestedDeviceName = require('os').hostname() || 'Headless';
-			rl.question("Please name this device ["+suggestedDeviceName+"]: ", function(deviceName){
-				if (!deviceName)
-					deviceName = suggestedDeviceName;
-				var userConfFile = appDataDir + '/conf.json';
-				fs.writeFile(userConfFile, JSON.stringify({deviceName: deviceName, admin_email: "admin@example.com", from_email: "noreply@example.com"}, null, '\t'), 'utf8', function(err){
-					if (err)
-						throw Error('failed to write conf.json: '+err);
-					// rl.question(
-					console.log('Device name saved to '+userConfFile+', you can edit it later if you like.\n\nPassphrase for your private keys: ')
-						// function(passphrase){
-							// rl.close();
-					var passphrase = ""
-					if (process.stdout.moveCursor) process.stdout.moveCursor(0, -1);
-					if (process.stdout.clearLine)  process.stdout.clearLine();
-					var deviceTempPrivKey = crypto.randomBytes(32);
-					var devicePrevTempPrivKey = crypto.randomBytes(32);
-
-					var mnemonic = new Mnemonic(); // generates new mnemonic
-					while (!Mnemonic.isValid(mnemonic.toString()))
-						mnemonic = new Mnemonic();
-
-					writeKeys(mnemonic.phrase, deviceTempPrivKey, devicePrevTempPrivKey, function(){
-						console.log('keys created');
-						var xPrivKey = mnemonic.toHDPrivateKey(passphrase);
-						createWallet(xPrivKey, function(){
-							onDone(mnemonic.phrase, passphrase, deviceTempPrivKey, devicePrevTempPrivKey);
-						});
-					});
-						// }
-					// );
-				});
-			});
-		}
-		else{ // 2nd or later start
-			// rl.question("Passphrase: ", function(passphrase){
-			var passphrase = "";
-			rl.close();
-			if (process.stdout.moveCursor) process.stdout.moveCursor(0, -1);
-			if (process.stdout.clearLine)  process.stdout.clearLine();
-			var keys = JSON.parse(data);
-			var deviceTempPrivKey = Buffer(keys.temp_priv_key, 'base64');
-			var devicePrevTempPrivKey = Buffer(keys.prev_temp_priv_key, 'base64');
-			determineIfWalletExists(function(bWalletExists){
-				if (bWalletExists)
-					onDone(keys.mnemonic_phrase, passphrase, deviceTempPrivKey, devicePrevTempPrivKey);
-				else{
-					var mnemonic = new Mnemonic(keys.mnemonic_phrase);
-					var xPrivKey = mnemonic.toHDPrivateKey(passphrase);
-					createWallet(xPrivKey, function(){
-						onDone(keys.mnemonic_phrase, passphrase, deviceTempPrivKey, devicePrevTempPrivKey);
-					});
-				}
-			});
-			// });
-		}
-	});
-}
-
-function writeKeys(mnemonic_phrase, deviceTempPrivKey, devicePrevTempPrivKey, onDone){
-	var keys = {
-		mnemonic_phrase: mnemonic_phrase,
-		temp_priv_key: deviceTempPrivKey.toString('base64'),
-		prev_temp_priv_key: devicePrevTempPrivKey.toString('base64')
-	};
-	fs.writeFile(KEYS_FILENAME, JSON.stringify(keys, null, '\t'), 'utf8', function(err){
-		if (err)
-			throw Error("failed to write keys file");
-		if (onDone)
-			onDone();
-	});
-}
-
-function createWallet(xPrivKey, onDone){
-	var devicePrivKey = xPrivKey.derive("m/1'").privateKey.bn.toBuffer({size:32});
-	var device = require('trustnote-pow-common/wallet/device.js');
-	device.setDevicePrivateKey(devicePrivKey); // we need device address before creating a wallet
-	var strXPubKey = Bitcore.HDPublicKey(xPrivKey.derive("m/44'/0'/0'")).toString();
-	var walletDefinedByKeys = require('trustnote-pow-common/wallet/wallet_defined_by_keys.js');
-	walletDefinedByKeys.createWalletByDevices(strXPubKey, 0, 1, [], 'any walletName', function(wallet_id){
-		walletDefinedByKeys.issueNextAddress(wallet_id, 0, function(){
-			onDone();
-		});
-	});
 }
 
 function readSingleAddress(handleAddress){
@@ -166,14 +59,6 @@ function readSingleWallet(handleWallet){
 		if (rows.length > 1)
 			throw Error("more than 1 wallet");
 		handleWallet(rows[0].wallet);
-	});
-}
-
-function determineIfWalletExists(handleResult){
-	db.query("SELECT wallet FROM wallets", function(rows){
-		if (rows.length > 1)
-			throw Error("more than 1 wallet");
-		handleResult(rows.length > 0);
 	});
 }
 
@@ -517,7 +402,7 @@ function checkRoundAndComposeCoinbase(round_index) {
 }
 
 setTimeout(function(){
-	readKeys(function(mnemonic_phrase, passphrase){
+	signer.readKeys(function(mnemonic_phrase, passphrase){
 		var mnemonic = new Mnemonic(mnemonic_phrase);
 		// global
 		xPrivKey = mnemonic.toHDPrivateKey(passphrase);
